18d87aff80277579b352e68b55b583da5fdc9a2c
Dragan Ceran <dragandceran@gmail.com>
2019-09-12 13:01:41 +0100

    image resize

--- src/components/about.js
+++ src/components/about.js
@@ -5,8 +5,7 @@ const About = () => {
     <div id="aboutme" className="section aboutme ">
       <h1 className="subtitle has-text-weight-bold">ABOUT ME</h1>
       <div className="content">
-        <p>From Commodore 64 to MacBook Pro, computers were a big part of my life,initially just for fun and now used professionally. Rise of world wide web and endless possibilities that have arisen was something to be part of it and I want myself in
-        it. Switching career is a big step, filled with unknown, excitement and something which will make dream come true.
+        <p>From commodore 64 till today I always wanted to do more with computers, I wanted to create something  that  will  bring some  benefit, fun and joy to all users. Prevented  from coding ( yes, life happens) I had  reached a point where I can devote myself to web development and professional growth. Right now I am looking to join a team of like-minded developers where I can contribute and continue professional development.
         </p>
       </div>
     </div>
